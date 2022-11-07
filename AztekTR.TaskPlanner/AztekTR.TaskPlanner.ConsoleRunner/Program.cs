de5e2c6269e5165b16fb6548c04a187c792f4a43
AztekTR <rubickaghanims@gmail.com>
2022-11-07 14:55:35 +0200

    feat: init

---
+++ AztekTR.TaskPlanner/AztekTR.TaskPlanner.ConsoleRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AztekTR.TaskPlanner.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
